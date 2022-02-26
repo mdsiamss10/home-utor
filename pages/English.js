d9a9a2acbbb34bd9a408a0bb4726c8bbe5840748
mdsiamss10 <mdsiamss10@gmail.com>
2022-02-27 01:07:31 +0600

    hello

--- pages/English.js
+++ pages/English.js
@@ -9,10 +9,10 @@ function English() {
         <div className={styles.hw_1}>
         <h2>26/2/2022</h2>
         <ul>
-          <li>339 page 1,2</li>
-          <li>345 page 1,2</li>
-          <li>348 page 1,2</li>
-          <li>286 page Abolish - Entertain</li>
+          <li>339 page 1,2. (Advance)</li>
+          <li>345 page 1,2. (Advance)</li>
+          <li>348 page 1,2. (Advance)</li>
+          <li>286 page Abolish - Entertain. (Advance)</li>
         </ul>
         <hr/>
         </div>
