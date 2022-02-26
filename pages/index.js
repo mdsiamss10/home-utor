ae8e1a97efcc876d6ef49efc1393ee5de878c3e1
mdsiamss10 <94430474+mdsiamss10@users.noreply.github.com>
2022-02-27 00:26:46 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styles from '../styles/Home.module.css'
-import Header from './header';
+import Header from './Header';
 export default function Home() {
   return (
     <div className={styles.div}>
