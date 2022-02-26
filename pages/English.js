72b1f75ac9b4e43dc0d227d52f9baa065c3b8e00
mdsiamss10 <mdsiamss10@gmail.com>
2022-02-27 01:16:32 +0600

    j

--- pages/English.js
+++ pages/English.js
@@ -6,16 +6,18 @@ function English() {
           <h1 className={styles.name}>SIAM</h1>
       </nav>
       <div className={styles.box}>
+
         <div className={styles.hw_1}>
-        <h2>26/2/2022</h2>
-        <ul>
-          <li>339 page 1,2. (Advance)</li>
-          <li>345 page 1,2. (Advance)</li>
-          <li>348 page 1,2. (Advance)</li>
-          <li>286 page Abolish - Entertain. (Advance)</li>
-        </ul>
-        <hr/>
+          <h2>26/2/2022</h2>
+          <ul>
+            <li>339 page 1,2. (Advance)</li>
+            <li>345 page 1,2. (Advance)</li>
+            <li>348 page 1,2. (Advance)</li>
+            <li>286 page Abolish - Entertain. (Advance)</li>
+          </ul>
+          <hr/>
         </div>
+
       </div>
     </div>
   )
